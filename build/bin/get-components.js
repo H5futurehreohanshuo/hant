57678bf32eee1e26db58cf47cff1eedad4d24e8c
hanshuo <https://github.com/H5futurehreohanshuo>
2018-06-06 16:14:23 +0800

    继续编写build-entry.js文件代码

--- build/bin/get-components.js
+++ build/bin/get-components.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-// 输出 packages 下面所有的文件夹 (除了 excludes 中的元素)
+// 输出 packages 下面一级目录所有的文件夹 (排除 excludes 中的元素)
 module.exports = function() {
   const dirs = fs.readdirSync(path.resolve(__dirname, '../../packages'));
   const excludes = ['index.js', 'vant-css', 'mixins', 'utils', '.DS_Store'];
