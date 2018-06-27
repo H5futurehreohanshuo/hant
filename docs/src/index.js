b533efe08115a824c76e65068dc95d590de3899a
韩硕 <18832643723@163.com>
2018-06-27 08:31:38 +0800

    增加 html 模板文件

--- docs/src/index.js
+++ docs/src/index.js
@@ -14,28 +14,37 @@ const router = new VueRouter({
   routes: routes()
 });
 
+// 路由跳转前
 router.beforeEach((route, redirect, next) => {
+  // 手机预览的 demo 页面要 replace 路由地址
   if (isMobile) {
     location.replace('mobile.html' + location.hash);
   }
+  // 开启加载进度条
   progress.start();
+  // 更改页面 title
   document.title = route.meta.title || document.title;
   next();
 });
 
+// 路由加载后
 router.afterEach(() => {
+  // 关闭进度条
   progress.done();
+  // 页面滚动到最顶步
   window.scrollTo(0, 0);
+  // 同步主页面和 iframe 的路由状态
   Vue.nextTick(() => window.syncPath());
 });
 
+// 把 vueRouter 赋给 window，使得 iframe 中也能到 vue-router
 window.vueRouter = router;
 
 if (process.env.NODE_ENV !== 'production') {
   Vue.config.productionTip = false;
 }
 
-new Vue({
+new Vue({ // eslint-disable-line
   render: h => h(app),
   router,
   el: '#app'
